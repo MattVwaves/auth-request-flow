b5a54eb14ffabd8dfc1df54e5b75187c1b6ab6a9
wagnerAJS221 <wagneralex221@gmai.com>
2023-01-23 15:28:13 +0000

    2nd requirment completed

--- src/router.js
+++ src/router.js
@@ -1,24 +1,35 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
-secret = process.env.JWT_SECRET;
+const express = require('express')
+const jwt = require('jsonwebtoken')
+secret = process.env.JWT_SECRET
 
-const router = express.Router();
+const router = express.Router()
 
 const mockUser = {
-  username: "authguy",
-  password: "mypassword",
+  username: 'authguy',
+  password: 'mypassword',
   profile: {
-    firstName: "Chris",
-    lastName: "Wolstenholme",
-    age: 43,
-  },
-};
+    firstName: 'Chris',
+    lastName: 'Wolstenholme',
+    age: 43
+  }
+}
 
-router.post("/login", (req, res) => {
-  const token = jwt.sign(mockUser.username, secret);
-  res.json({ token });
-});
+router.post('/login', (req, res) => {
+  const token = jwt.sign(mockUser.username, secret)
+  res.json({ token })
+})
 
-router.get("/profile", (req, res) => {});
+router.get('/profile', (req, res) => {
+  const header = req.headers['authorization']
+  // console.log(header)
+  const token = header.split(' ')[1]
+  console.log(token)
+  try {
+    const validToken = jwt.verify(token, secret)
+    res.status(200).json(mockUser.profile)
+  } catch (error) {
+    res.status(400).json({ Error: 'Invalid token detected!' })
+  }
+})
 
-module.exports = router;
+module.exports = router
